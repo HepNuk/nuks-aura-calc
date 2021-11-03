5e8fbed885f097019ef5f0ce4443ededfce3206c
nikola.perotic.1 <nikola.perotic.1@ens.etsmtl.ca>
2021-11-03 17:38:56 -0400

    Added Services to RePoE for Aura Gems

---
+++ src/services/AurasServices.ts
@@ -0,0 +1,27 @@
+const REPOE: string = 'https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/';
+
+export default {
+  getAuras: async (): Promise<Array<any>> => {
+    const data: any = await fetch(REPOE + 'gems.json')
+    const json: any = await data.json();
+    
+    let auraArray = Object.values(json).filter((value: any) => {
+      // Filter out non-aura skills
+      if (!value.stat_translation_file.includes("/aura_skill")) return false;
+      // Filter out Vaal skills
+      if (value.active_skill.display_name.includes("Vaal")) return false;
+      // Filter out Royal skills
+      if (value.base_item.id.includes('Royal')) return false
+      // Filter out other skills that are classified as auras but are not buffing Auras
+      if (value.active_skill.id === 'blinding_aura' 
+        || value.active_skill.id === 'chaos_degen_aura_unique'
+        || value.active_skill.id === 'vaal_aura_elemental_damage_healing'
+        || value.active_skill.id === 'blood_sand_armour') return false;
+
+      return true;
+    })
+
+    return auraArray;
+  }
+}
+
