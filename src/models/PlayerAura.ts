56b2d59ff0bf11c83b15440ac4fad03561c9db57
HepNuk <nikolaperotic@gmail.com>
2021-11-11 17:38:04 -0500

    Fix calculation and splifie code

--- src/models/PlayerAura.ts
+++ src/models/PlayerAura.ts
@@ -1,6 +1,15 @@
 import Aura from './Aura';
 import SupportGem from './SupportGem';
 
+const STAT_REGEX = [
+  /You and nearby (?:A|a)llies (?:have |gain |deal )?([a-zA-Z0-9{ }%]*)/
+];
+
+const FILTER = {
+  youAndNearby: /You and (?:N|n)earby (?:A|a)llies/,
+  nearbyEnemies: /(:?N|n)earby (?:E|e)nemies (?:have|take)/,
+}
+
 export default class PlayerAura {
   public id: string;
   public displayName: string;
@@ -35,20 +44,24 @@ export default class PlayerAura {
     const stats: string[] = [];
     this.auraDetails.stats.forEach((statLine, i) => {
       let line: string = statLine;
+      if (FILTER.youAndNearby.test(line) || FILTER.nearbyEnemies.test(line)) {
+        if (this.auraDetails.statsPerLevel[i].value) {
+          const isIntegere = !(/% Regenerate/.test(line));
+          const statValue: number = this.scaleStatWithAuraEffect(this.auraDetails.statsPerLevel[i].value, ae, isIntegere);
 
-      if (this.auraDetails.statsPerLevel[i].value) {
-        const statValue: number = this.auraDetails.statsPerLevel[i].value * (1 + (ae / 100));
-        if (statValue < 0) line = this.handleNegativeValues(0, line);
-        line = line.replace('{0}', Math.abs(statValue).toString());
+          line = this.youAndNearbyAlliesStatLine(line, statValue, i);
 
-      } else {
-        this.auraDetails.statsPerLevel[i][level - 1].forEach((e: any, j: number) => {
-          const statValue: number = e.value * (1 + (ae / 100));
-          if (statValue < 0) line = this.handleNegativeValues(i, line);
-          line = line.replace(`{${j}}`, Math.abs(statValue).toString());
-        });
+        } else {
+          this.auraDetails.statsPerLevel[i][level - 1].forEach((e: any, j: number) => {
+            const isIntegere = !(/% Regenerate/.test(line));
+            const statValue: number = this.scaleStatWithAuraEffect(e.value, ae, isIntegere);
+
+            line = this.youAndNearbyAlliesStatLine(line, statValue, i, j);
+
+          });
+        }
+        stats.push(line);
       }
-      stats.push(line);
     });
 
     return stats;
@@ -60,16 +73,16 @@ export default class PlayerAura {
     const ae: number = this.auraEffectForGem(auraEffect, supportGems);
     const qualityStats: string[] = [];
     const aQual = this.altQuality;
+
     this.auraDetails.qualityStats[aQual].forEach((qualityLine, i) => {
-      if (/You and nearby (?:A|a)llies/.test(qualityLine)) {
-        let statValue: number = (this.auraDetails.statsPerQuality[aQual][i] / 100) * (1 + (ae / 100)) / 20 * this.quality;
-        let line = qualityLine;
-        if (statValue < 0) line = this.handleNegativeValues(0, qualityLine);
-        statValue =  Math.floor(Math.abs(statValue));
-        if (statValue > 0) qualityStats.push(line.replace('{0}', Math.abs(statValue).toString()));
+      if (FILTER.youAndNearby.test(qualityLine) || FILTER.nearbyEnemies.test(qualityLine)) {
+        const isIntegere = !(/% Regenerate/.test(qualityLine));
+        const statValue: number = this.scaleStatWithAuraEffect(this.auraDetails.statsPerQuality[aQual][i], ae, isIntegere, this.quality);
+        const line = this.youAndNearbyAlliesQualityLine(qualityLine, statValue, i, aQual);
+
+        if (line) qualityStats.push(line);
       }
     });
-
     return qualityStats;
   }
 
@@ -119,5 +132,50 @@ export default class PlayerAura {
     }
 
     return line;
+  }
+
+  private filterYouAndNearbyAllies(line: string): string {
+    const filtered = line.match(STAT_REGEX[0]);
+
+    if (filtered) {
+      if (filtered[1].includes('to Accuracy Rating')) return filtered[1].replace('to', 'additional'); // Fix for precision.
+      return filtered[1];
+    }
+    return line;
+  }
+
+  private youAndNearbyAlliesStatLine(statLine: string, statValue: number, i: number, j: number = 0): string{
+    if (statValue < 0) statLine = this.handleNegativeValues(i, statLine);
+    statLine = this.filterYouAndNearbyAllies(statLine);
+    statValue = Math.abs(statValue);
+    statLine = statLine.replace(`{${j}}`, (statValue).toString());
+    if (statValue > 0) return statLine.replace('{j}', (statValue).toString());
+    return null;
+  }
+
+  private youAndNearbyAlliesQualityLine(qualityLine: string, statValue: number, i: number, aQual: number = null): string{
+    let line = this.filterYouAndNearbyAllies(qualityLine);
+    if (statValue < 0) line = this.handleNegativeValues(0, line);
+    statValue =  Math.abs(statValue);
+    if (statValue > 0) return line.replace('{0}', (statValue).toString());
+    return null;
+  }
+
+  private scaleStatWithAuraEffect(statValue: number, ae: number, integere: boolean, quality: number = null) {
+    // Quality Values are stored as Integers, we have to divide by 1000 to get their true value per 1% quality
+    if (quality) statValue = statValue / 1000.0;
+
+    // Auraeffect formula
+    const auraMultiploer = (1 + (ae / 100.0));
+    const value = statValue * auraMultiploer * (quality ? quality : 1);
+
+    if (integere) return Math.floor(value);
+    console.log(this.roundTo(value, 2))
+    return this.roundTo(value, 2);
+  }
+
+  private roundTo(num: number, to: number) {
+    const m = (to <= 0) ? 1 : 10**to;
+    return Math.floor(num * m)/m;
   }
 }
